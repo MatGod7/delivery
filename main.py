e9233307e5df905b8d7a4846964cc073675b76a7
Matias G <mdgodoy12@hotmail.com>
2023-10-08 17:39:59 +0000

    agregando pandas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #push
 from flask import Flask
 from flask import jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -18,6 +19,20 @@ def name(value):
 def bob():
     val = {"value": "bob"}
     return jsonify(val)
+
+@app.route('/html')
+def html():
+    """Returns some custom HTML"""
+    return """
+    <title>This is a Hello World World Page</title>
+    <p>Hello</p>
+    <p><b>World</b></p>
+    """
+
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
 
 
 if __name__ == '__main__':
